c120dbeeb60218a60b272b147fe919eddc31510c
Hassan Nazar <hnazar@redbridge.se>
2019-08-04 15:50:00 +0200

    - Prometheus shell script and base config added

---
+++ prometheus-init.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+promConfig="$(pwd)/prometheus.yml"
+promContainerName="prometheus-server"
+echo "Initializing Prometheus"
+if test -f "$promConfig"; then
+    echo "Found prometheus.yml file in working dir"
+
+    read -p "This will remove stored docker container w. name: $promContainerName, Proceed? " -n 1 -r
+    echo    # move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      echo "Attempting to remove any previous containers"
+      docker container rm -f $promContainerName
+      echo "Attempting to start prometheus using docker on port 9090"
+      docker container run -d -p 9090:9090 -v $promConfig:/etc/prometheus/prometheus.yml --name $promContainerName \
+        prom/prometheus
+    else
+      echo "Aborting..."
+    fi
+
+    
+    echo "Edit prometheus.yml for config-changes!"
+    echo "Prometheus server started, hit CTRL+C to exit"
+    trap ctrl_c INT
+
+    function ctrl_c() {
+      echo "Stopping Prometheus"
+      docker stop $promContainerName
+      exit 0
+    }
+
+    # Wait forever
+    read -r -d '' _ </dev/tty
+else
+    echo "No prometheus.yml file found in working dir! Aborting..."
+fi
